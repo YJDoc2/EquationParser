08dc340704ac965aa832a597ea9ddf24c92b2188
Yashodhan Joshi <yashsumedha@gmail.com>
2019-08-14 21:28:28 +0530

    Initial commit

---
+++ valparser.c
@@ -0,0 +1,65 @@
+#include "./stack.c"
+#include<string.h>
+
+char infix[250];
+
+void prepParse(){
+
+    char pre;
+    int i =0,j=0;
+    if(in[i]=='-'){
+        infix[j++] = '(';
+        infix[j++] = '0';
+        infix[j++] = '-';
+        infix[j++] = in[++i];
+        infix[j++] = ')';
+        i++;
+    }
+    while(in[i]!='\0'){
+        if(isdigit(in[i])||in[i] =='.'){
+            infix[j] = in[i];
+            i++;
+            j++;
+            pre = in[i];
+        }else if(in[i] == '*' && in[i+1] == '*'){
+            infix[j] = 'E';
+            j++;i+=2;
+            pre = in[i];
+        }else if(in[i] == '-' && (pre == '+'||pre == '-'||pre == '*'||pre == '/'||pre == '%'||pre == 'e'||pre == 'E')){
+            infix[j++] = '(';
+            infix[j++] = '0';
+            infix[j++] = '-';
+            infix[j++] = in[++i];
+            infix[j++] = ')';
+            i++;
+        }else{
+            infix[j] = in[i];
+            j++;i++;
+        }
+
+
+    }
+
+
+
+}
+
+float parseVal(int k){
+    int dec =0;
+    float val = 0.0;
+    while(isdigit(infix[k])){
+        val = val*10 + (float)(infix[k]-'0');
+        k++;
+    }
+   
+    if(infix[k] == '.'){
+         k++;
+    while(isdigit(infix[k])){
+        val = val*10 + (float)(infix[k]-'0');
+        k++;
+        dec++;
+    }}
+    val = nextafterf(val/pow(10,dec),val+1);
+    pushVal(val);
+    return k;
+}
