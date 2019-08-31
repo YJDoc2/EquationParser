b5bb63649525a3e1280685a2dcf3897c6848ad21
YatharthVyas <yatharth2000.vyas@gmail.com>
2019-08-31 20:10:15 +0530

    Added gQueue.c

--- [Work in Progress]Yatharth/gQueue.c
+++ [Work in Progress]Yatharth/gQueue.c
@@ -8,9 +8,65 @@ typedef struct varQueue
 	float var;
 	struct varQueue *next;
 }gQueue;
-gQueue *top,*rear,*temp;
-void Push()
+gQueue *front,*rear,*temp;
+void push(float variable)
 {
 	gQueue *newnode = (gQueue *)malloc(sizeof(gQueue));
-
+	newnode->next=NULL;
+	newnode->var=variable;
+	if (front==NULL)
+	{
+		front=newnode;
+		rear=newnode;
+	}
+	else
+	{
+		rear->next=newnode;
+		rear=newnode;
+	}
+}
+float pop()
+{
+	if (front==NULL)
+	{
+		printf("Empty Queue\n");
+		return 0;
+	}
+	else
+	{
+		float a;
+		a=front->var;
+		temp=front;
+		front=front->next;
+		printf("%f \n",a);
+		free(temp);
+		return a;
+	}
+}
+void display()
+{
+	temp=front;
+	if (front==NULL)
+	{
+		printf("Queue is empty \n");
+	}
+	else
+	{
+		while (temp!=NULL)
+		{
+			printf("%f \n",temp->var);			//Display the variables from top -> bottom
+			temp=temp->next;
+		}
+	}
+}
+//For debugging
+void main()
+{
+	printf("Input is 1 , 2 , 3 , 4.5\n");
+	push(1);
+	push(2);
+	push(3);
+	push(4.5);
+	printf("pop=%f\n",pop() );
+	display();
 }
