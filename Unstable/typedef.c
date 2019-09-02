01cc2a4f88ab3c27a6cca501c14bbaa2b428e043
Yashodhan Joshi <yashsumedha@gmail.com>
2019-09-02 11:13:29 +0530

    Unstable added

---
+++ Unstable/typedef.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#ifndef TYPEDEF
+#define TYPEDEF
+
+
+typedef enum
+{
+    SUCCESS,ERROR,VARIABLE_NOT_DEFINED,OUT_OF_BOUND,FILE_ERROR
+}resultStatus;
+
+
+typedef struct result
+{
+    float data;
+    float subdata[5];
+    resultStatus status;
+    char error_info[100];
+}Result;
+typedef struct var
+{
+    float val;
+    char name[30];
+    char info[30];
+    struct var *next;
+}Var;
+
+#endif
