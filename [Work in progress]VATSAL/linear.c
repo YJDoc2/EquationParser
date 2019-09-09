3b2bf994e8af12cd33754600cd85c7b5948d6650
Vatsalsoni13 <53573748+Vatsalsoni13@users.noreply.github.com>
2019-09-09 23:11:42 +0530

    Update linear.c

--- [Work in progress]VATSAL/linear.c
+++ [Work in progress]VATSAL/linear.c
@@ -1,14 +1,34 @@
-//A little Rectification needed.
-
 
 #include<stdio.h>
-int main()
+
+float deter(int n,float M[20][20])//<<Function for Finding the Determinanat!>>
 {
-    int i,j,k,n;
-    float A[20][20],c,X[10],sum=0.0;
-    printf("Enter th order of rows: ");
+    int i,j,k;
+    float ratio,det=0.0;
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++){
+            if(j>i){
+                ratio = M[j][i]/M[i][i];
+                for(k = 1; k <= n; k++){
+                    M[j][k] -= ratio * M[i][k];
+                }
+            }
+        }
+    }
+    det = 1; //storage for determinant
+    for(i = 1; i <=n; i++)
+        det *= M[i][i];
+    printf("The determinant of matrix is: %.2f\n\n", det);
+    return det;
+}
+int main()
+
+{
+    int i,j,k,n,d,e,f;
+    float A[20][20],c,X[10],sum=0.0,res=0.0,M[20][20];
+    printf("Enter co-efficients of variables and constant at last.\nStd.Equations:\na1x1 + b1x2 +c1x3+...=d1\na2x1 + b2x2 +c2x3+...=d2\n.\n.\n.\n.\nanx1 + bnx2 +cnx3+...=dn\n\nEnter th order of rows: ");
     scanf("%d",&n);
-    printf("Enter the value of matrix: ");
+    printf("Enter the value of matrix: \n");
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=(n+1);j++)
@@ -17,7 +37,14 @@ int main()
             scanf("%f",&A[i][j]);
         }
     }
-    for(i = 1; i <= n; i++){
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            M[i][j]=A[i][j];
+        }
+    }
+  /* for(i = 1; i <= n; i++){
         for(j = 1; j <= n; j++){
             if(j>i){
                 ratio = A[j][i]/A[i][i];
@@ -30,9 +57,10 @@ int main()
     det = 1; //storage for determinant
     for(i = 1; i <=n; i++)
         det *= A[i][i];
-    printf("The determinant of matrix is: %.2f\n\n", det);
-    if(det!=0)
-    {
+    printf("The determinant of matrix is: %.2f\n\n", det);*/
+    res=deter(n,M);
+    if(res!=0)
+    {//<<Elimination Starts>>For making upper triangular matrix
     for(j=1;j<=n;j++)
     {
         for(i=1;i<=n;i++)
@@ -56,7 +84,7 @@ int main()
         }
         printf("\n");
     }*/
-    X[n]=A[n][n+1]/A[n][n];
+    X[n]=A[n][n+1]/A[n][n];//<<Back Substitution>>
     for(i=n-1;i>=1;i--)
     {
         sum=0.0;
@@ -71,8 +99,8 @@ int main()
     {
         printf("\n%f\n",X[i]);
     }
+    return 0;
     }
     else
         printf("*****Infinite or No Solutions!****");
-    return 0;
 }
