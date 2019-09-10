da345c40c50bc9b99fb49c8b67f8a52fad17bbb4
YatharthVyas <yatharth2000.vyas@gmail.com>
2019-09-10 20:03:30 +0530

    poly.c is now working and giving approximate output, Will make it better and add more improvements

--- [Work in Progress]Yatharth/poly.c
+++ [Work in Progress]Yatharth/poly.c
@@ -2,10 +2,11 @@
 #include<stdlib.h>
 #include<math.h>
 #include"./polydiv.c"
+float S;
 float root()
 {
 	float t;
-	t=(a[1]/a[0])*(a[1]/a[0])-2*(a[2]/a[0]);
+	t=(a[1]/a[0])*(a[1]/a[0])-2*(a[2]/a[0]);				//Upper Limit of Root
 	if (t<0)
 	{
 		printf("Error Roots Not Defined");
@@ -16,9 +17,44 @@ float root()
 	}
 	return t;
 }
+void getSum()
+{
+	int i;
+	S=0;
+	for (i=0;i<n;i++)
+	{
+		S=S+b[i];
+	}
+}
 void main()
 {
+	int i,j;
 	getPoly();
 	r=root();
-	printf("%f\n", r);
+	R=0;		// For Line 42 Initialization
+	S=1;		// For Line 42 Initialization
+	for (i = 0; i < n; i++)
+	{
+		if (n-i>1)	// For all cases except Last when the eqn will be linear
+		{
+			do
+			{
+				r=r-R/S;
+				R=polyDiv();
+				getSum();
+				//printf("%f R=%f\n", r,R);
+			}
+			while (R>0.001);				//Iterating the value of Root till Remainder is less than 0.001
+			for (j=0; j < n-i; j++)			//Storing Quotient Array in our next Divisor Array
+			{
+				a[j]=b[j];
+			}
+			printf("Root %d = %f\n",i+1, r);
+		}
+		else							//When the equation is reduced to Linear Form
+		{
+			r = -a[1]/a[0];
+			printf("Root %d = %f \n",i+1,r);
+		}
+	}
 }
