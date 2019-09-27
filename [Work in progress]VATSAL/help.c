a753491fc2821c52430e67c8586a22be0d59f39e
Vatsalsoni13 <53573748+Vatsalsoni13@users.noreply.github.com>
2019-09-27 21:47:44 +0530

    Add files via upload

---
+++ [Work in progress]VATSAL/help.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    printf("===========================================================================================================================================================================\n");
+    printf("                                                                                   HELP                                                                                            \n");
+    printf("===========================================================================================================================================================================\n");
+    printf(">>>>>>LINEAR EQUATIONS:\n1.STDANDARD EQUATIONS: \n----->a1x1 + a2x2 + a3x3 + a4x4+... = b\n----->Enter co-efficients first and constant at the last equation by equation.\n");
+    printf("----->In case of 0 as co-efficient or constant write 1*10^-6 to 1*10^-36.  Example: 0.0000000000001\n----->If determinant is zero 0 then linear system of equations is not consistent, therefore solutions will not exits or will have infinite no. of solutions.\n");
+    printf("----->If determinant is not zero then linear system of equations is consistent and equations can have unique solution.\n");
+    printf("----->Method used for solving the linear equations is Gauss-Elimination Method.");
+}
