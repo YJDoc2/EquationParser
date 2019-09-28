6a548a1e2dc460f882f2307d55ffa2062b94ace5
YatharthVyas <yatharth2000.vyas@gmail.com>
2019-09-28 10:53:45 +0530

    Updated help.c

--- [Work in progress]VATSAL/help.c
+++ [Work in progress]VATSAL/help.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
 void main()
 {
-    printf("===========================================================================================================================================================================\n");
-    printf("                                                                                   HELP                                                                                            \n");
-    printf("===========================================================================================================================================================================\n");
-    printf(">>>>>>LINEAR EQUATIONS:\n1.STDANDARD EQUATIONS: \n----->a1x1 + a2x2 + a3x3 + a4x4+... = b\n----->Enter co-efficients first and constant at the last equation by equation.\n");
+
+  printf(" ______                  _   _               _____                       		 \n");
+  printf("|  ____|                | | (_)             |  __ \\                       		 \n");
+  printf("| |__   __ _ _   _  __ _| |_ _  ___  _ __   | |__) |_ _ _ __ ___  ___ _ __  		\n");
+  printf("|  __| / _` | | | |/ _` | __| |/ _ \\| '_ \\  |  ___/ _` | '__/ __|/ _ \\ '__| 	\n");
+  printf("| |___| (_| | |_| | (_| | |_| | (_) | | | | | |  | (_| | |  \\__ \\  __/ |    	\n");
+  printf("|______\\__, |\\__,_|\\__,_|\\__|_|\\___/|_| |_| |_|   \\__,_|_|  |___/\\___|_|  	  \n");
+  printf("          | |                                                           			  \n");
+  printf("          |_|                                                           	 		  \n");
+
+    printf("========================================================================================================================\n");
+    printf("                                             		    HELP                                                                                            \n");
+    printf("========================================================================================================================\n");
+    printf(">>>>>>LINEAR EQUATIONS:\n1.STANDARD EQUATIONS: \n----->a1x1 + a2x2 + a3x3 + a4x4+... = b\n----->Enter co-efficients first and constant at the last equation by equation.\n");
     printf("----->In case of 0 as co-efficient or constant write 1*10^-6 to 1*10^-36.  Example: 0.0000000000001\n----->If determinant is zero 0 then linear system of equations is not consistent, therefore solutions will not exits or will have infinite no. of solutions.\n");
     printf("----->If determinant is not zero then linear system of equations is consistent and equations can have unique solution.\n");
     printf("----->Method used for solving the linear equations is Gauss-Elimination Method.");
